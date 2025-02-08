a5a3340418c5bcaaba405943c8671a8a7ed95da6
Dmitry Petrovich <dmitrii.petrovich777@mail.ru>
2025-02-08 23:03:18 +0300

    cleared of errors file

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from typing import List,Dict
+from typing import List, Dict
 
 
-def filter_by_state(handling_data: list[dict], state: str = "EXECUTED") -> list:
+def filter_by_state(handling_data: List[Dict], state: str = "EXECUTED") -> List:
     """Функция обработки данных с возвратом  списка по значению"""
     new_list = []
     for i in handling_data:
@@ -10,13 +10,14 @@ def filter_by_state(handling_data: list[dict], state: str = "EXECUTED") -> list:
     return new_list
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],state="CANCELED"))
-#print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],))
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state="CANCELED"))
 
 
-def sort_by_date(date_info: list[dict], sort_key: bool=True) -> list[dict]:
+def sort_by_date(date_info: List[Dict], sort_key: bool = True) -> List[Dict]:
     """Функция сортировки данных по дате"""
-    sort_list = sorted(date_info, key=lambda x:x["date"],reverse=sort_key)
+    sort_list = sorted(date_info, key=lambda x: x["date"], reverse=sort_key)
     return sort_list
+
 
 print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
