5cc03a5733a295e1231c2db8716ac4de8ff8269d
Dmitry Petrovich <dmitrii.petrovich777@mail.ru>
2025-02-16 23:59:09 +0300

    add test_widget and new widget

--- Tests/test_widget.py
+++ Tests/test_widget.py
@@ -1,1 +1,48 @@
 import pytest
+from src.widget import mask_account_card, get_date
+
+
+@pytest.mark.parametrize("data_card_account, expected",
+                         [("Maestro 1596837868705199", "Maestro  1596 83** **** 5199"),
+                          ("Mastercard 7158300734726758", "Mastercard  7158 30** **** 6758"),
+                          ("Счет 64686473678894779589", "Счет  **9589")])  # Проверка правильности
+def test_mask_account_card_normal(data_card_account: str, expected: str):  # обработки данных
+    assert mask_account_card(data_card_account) == expected
+
+
+def test_mask_account_card_empty_list():   # Проверка пустого списка
+    with pytest.raises(ValueError):
+        mask_account_card("")
+
+
+def test_mask_account_card_number_invalid():   # Проверка на корректное количество цифр
+    with pytest.raises(ValueError):
+        mask_account_card("234456782345")
+
+
+def test_mask_account_card_name_invalid():   # Проверка на корректное имя
+    with pytest.raises(ValueError):
+        mask_account_card("Сче")
+
+
+@pytest.mark.parametrize("inform, expected",  # Проверка корректности обработки данных
+                         [("2024-03-11T02:26:18.671407", "11.03.2024"),
+                          ("2025-04-30T02:26:18.671407", "30.04.2025"),
+                          ("2012-12-31T02:26:18.671407", "31.12.2012")])
+def test_get_date_normal(inform: str, expected: str):
+    assert get_date(inform) == expected
+
+
+def test_get_date_empty_list():     # Проверка отработки при пустом списке
+    with pytest.raises(ValueError):
+        get_date("")
+
+
+def test_get_date_invalid_information():     # Проверка обработки при
+    with pytest.raises(ValueError):          # ошибке в количестве знаков
+        get_date("2024-03-T02:26:18.671407")
+
+
+def test_get_date_invalid_data():             # Проверка обработки при
+    with pytest.raises(ValueError):           # ошибке ввода даты
+        get_date("2024/03-T02:26:18.671407")
--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,22 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(data_card_account: str) -> str:
     """Маскировка карты или счета"""
+    if data_card_account == "":
+        raise ValueError("Введите название объекта")
     number_card = data_card_account.split()[-1]
+    name_bill = data_card_account.split()[0]
+    if len(number_card) != 16 and len(number_card) != 20:
+        raise ValueError("Неверно набрано количество цифр")
     if len(number_card) == 16:
         number_1 = get_mask_card_number(number_card)
         return f"{data_card_account[:-16]} {number_1}"
     elif len(number_card) == 20:
+        if name_bill != "Счет":
+            raise ValueError("Имя набрано неверно. Повторите попытку")
         number_2 = get_mask_account(number_card)
         return f"{data_card_account[:-20]} {number_2}"
     else:
-        return "Ошибка ввода данных. Повторите попытку. "
+        return "Введите корректные данные"
 
 
 print(mask_account_card("Maestro 1596837868705199"))
@@ -26,6 +33,14 @@ print(mask_account_card("Счет 73654108430135874305"))
 
 def get_date(information: str) -> str:
     """Возврат даты в формате ДД.ММ.ГГГГ"""
+    if information == "":
+        raise ValueError("Нет данных")
+    if len(information) != 26:
+        raise ValueError("Ошибка ввода количества знаков")
+    if information[4] != "-":
+        raise ValueError("Ошибка ввода даты")
+    if information[7] != "-":
+        raise ValueError("Ошибка ввода даты")
     data = information[0:10].split("-")
     return ".".join(data[::-1])
 
