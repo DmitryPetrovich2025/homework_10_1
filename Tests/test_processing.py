035e5c7a0f2d2012fc076c52b96d3fca4b63419e
Dmitry Petrovich <dmitrii.petrovich777@mail.ru>
2025-02-19 22:59:56 +0300

    add new_processing and test_processing

--- Tests/test_processing.py
+++ Tests/test_processing.py
@@ -1,1 +1,93 @@
 import pytest
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state_normal_1():  # Проверка при совпадении значения
+    assert (filter_by_state([{'id': 41428829, 'state': 'EXECUTED',
+                              'date': '2019-07-03T18:35:29.512364'}]) ==
+                            [{'id': 41428829, 'state': 'EXECUTED',
+                              'date': '2019-07-03T18:35:29.512364'}])
+
+
+def test_filter_by_state_norma():  # "Проверка при несовпадении значения
+    assert filter_by_state([{'id': 41428829, 'state': 'CANCELED',
+                             'date': '2019-07-03T18:35:29.512364'}]) == []
+
+
+def test_filter_by_state_empty_state():  # "Проверка при отсутствии значения
+    with pytest.raises(ValueError):
+        filter_by_state([{'id': 594226727, 'state': '',
+                          'date': '2018-09-12T21:27:25.241689'}])
+
+
+def test_filter_by_state_invalid_state():  # "Проверка при ошибке ввода значения
+    with pytest.raises(ValueError):
+        filter_by_state([{'id': 615064591, 'state': 'CANCEL',
+                          'date': '2018-10-14T08:21:33.419441'}])
+
+
+@pytest.fixture
+def low_date() -> list:
+    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-10-12T02:08:58.425572'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12T08:21:33.419441'}])
+
+
+@pytest.fixture
+def all_date():
+    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-10-12T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12T08:21:33.419441'}])
+
+
+@pytest.fixture
+def up_date() -> list:
+    return ([{'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12T08:21:33.419441'},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-10-12T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])
+
+
+@pytest.fixture
+def identical_date_in() -> list:
+    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-12-12T08:21:33.419441'}])
+
+
+@pytest.fixture
+def identical_date_out() -> list:
+    return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+             {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-12-12T08:21:33.419441'}])
+
+
+def test_sort_by_date_low(all_date: list, low_date: list):
+    # Проверка обработки в порядке убывания по дате
+    assert sort_by_date(all_date) == low_date
+
+
+def test_sort_by_date_up(all_date: list, up_date: list):
+    # Проверка обработки в порядке возрастания по дате
+    assert sort_by_date(all_date, sort_key=False) == up_date
+
+
+def test_sort_by_date_identical(identical_date_in: list, identical_date_out: list):
+    # Проверка обработки при наличии одинаковых дат
+    assert sort_by_date(identical_date_in) == identical_date_out
+
+
+def test_sort_by_date_empty():
+    # Проверка обработки при отсутствии даты
+    with pytest.raises(ValueError):
+        sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': ''}])
+
+
+def test_sort_by_date_invalid_date():
+    # Проверка обработки при некорректном вводе количества знаков в дате
+    with pytest.raises(ValueError):
+        sort_by_date([{'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12'}])
--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,13 @@ from typing import List, Dict
 
 
 def filter_by_state(handling_data: List[Dict], state: str = "EXECUTED") -> List:
-    """Функция обработки данных с возвратом  списка по значению"""
+    """Функция обработки данных с возвратом списка по значению"""
     new_list = []
     for i in handling_data:
+        if i.get("state") == "":
+            raise ValueError("Значение не обозначено")
+        if i.get("state") != "EXECUTED" and i.get("state") != "CANCELED":
+            raise ValueError("Значение неправильно введено")
         if i.get("state") == state:
             new_list.append(i)
     return new_list
@@ -16,19 +20,32 @@ print(filter_by_state([
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]))
+
+
 print(filter_by_state([
-    {'id':  41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-    state="CANCELED")
-)
+     {'id':  41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+      state="CANCELED"))
 
 
 def sort_by_date(date_info: List[Dict], sort_key: bool = True) -> List[Dict]:
     """Функция сортировки данных по дате"""
-    sort_list = sorted(date_info, key=lambda x: x["date"], reverse=sort_key)
-    return sort_list
+    for i in date_info:
+        if i.get("date") == "":
+            raise ValueError("Дата не введена")
+        if len(i.get("date")) != 26:
+                raise ValueError("Ошибка в количестве данных")
+    return sorted(date_info, key=lambda x: x["date"], reverse=sort_key)
+
+
+print(sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-12-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-10-12T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12T08:21:33.419441'}
+], sort_key=False))
 
 
 print(sort_by_date([
@@ -36,4 +53,12 @@ print(sort_by_date([
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-10-12T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-09-12T08:21:33.419441'}
+]))
+
+
+print(sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-12-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-12-12T08:21:33.419441'}
 ]))
