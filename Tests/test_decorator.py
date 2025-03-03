6c7863f2aa6ae240072c2c0577c9b0f4c7a25121
Dmitry Petrovich <dmitrii.petrovich777@mail.ru>
2025-03-03 23:55:46 +0300

    add test_decorator and readme.md

---
+++ Tests/test_decorator.py
@@ -0,0 +1,30 @@
+import pytest
+from src.decorators import my_function
+
+
+def test_my_function_type_error():         # Проверка выброса ошибки при
+    with pytest.raises(TypeError):         # некорректно введенном параметре
+        my_function(4, "e")          # при выводе результата в mylog.txt
+
+
+def test_my_function_positive():            # Проверка правильности обработки
+    positive = my_function(4, 2)      # данных при корректных входных данных
+    assert positive == 2
+
+
+def test_my_function_log(capsys):            # Проверка вывода в консоль при
+    my_function(0, 3)                  # записи в файл mylog.txt
+    captured = capsys.readouterr()
+    assert "" in captured.out
+
+
+def test_my_function_zero_invalid():         # Проверка выброса ошибки
+    with pytest.raises(ZeroDivisionError):   # при делении на "0"
+        my_function(2, 0)
+
+
+def test_result_my_function_in_file():       # Проверка логирования
+    my_function(2, 4)                  # результата в файл
+    with open("mylog.txt", "r", encoding="utf-8") as file:
+        logs = file.readlines()
+        assert "my_function ok\n" in logs[-1]
---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+from collections.abc import Callable
+from typing import Optional
+
+
+def log(filename: Optional[str] = None) -> Callable:
+    """
+    Декоратор, принимающий аргумент "filename" и записывающий
+    логи в файл(filename задан) или выводящий лог в консоль
+    (filename не задан).
+    """
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
+            except Exception as e:
+                if filename:
+                    error_type = type(e).__name__
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {error_type}. Inputs {args}, {kwargs}\n")
+                else:
+                    error_type = type(e).__name__
+                    file.write(f"{func.__name__} error: {error_type}. Inputs {args}, {kwargs}\n")
+                raise e
+        return wrapper
+    return decorator
+
+
+@log(filename="../mylog.txt")
+def my_function(x: int, y: int) -> float:
+    return x / y
+
+
+print(my_function(2, 4))
