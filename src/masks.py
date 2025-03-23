365a5a265ffe689b74c5a57d4fbca0f570aed872
Dmitry Petrovich <dmitrii.petrovich777@mail.ru>
2025-03-23 14:51:27 +0300

    Add logging for masks and utils, add file logs, new date in README

--- src/masks.py
+++ src/masks.py
@@ -1,29 +1,60 @@
+import logging
+
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="UTF-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(number_card: str) -> str:
     """Функция маскировки номера карты"""
+    logger.info("Начало работы программы маскировки номера карты.")
     if len(number_card) == 0:
-        raise ValueError("Номер карты не набран")
+        error_message = "Номер карты не набран"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if len(number_card) > 16 or len(number_card) < 16:
-        raise ValueError("Ошибка в количестве цифр")
+        error_message = "Ошибка в количестве цифр"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if not number_card.isdigit():
-        raise ValueError("Введены не только цифры")
+        error_message = "Введены не только цифры"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if len(number_card) == 16:
+        logger.info("Процесс маскировки карты. Окончание работы программы.")
         return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
     else:
-        return "Неверно набран номер карты. Повторите попытку."
+        error_message = "Неверно набран номер карты. Повторите попытку."
+        logger.error(error_message)
+        return error_message
 
 
 def get_mask_account(number_account: str) -> str:
     """Функция маскировки номера счёта"""
+    logger.info("Начало работы программы маскировки номера счёта.")
     if len(number_account) > 20:
-        raise ValueError("Количество цифр больше 20")
+        error_message = "Количество цифр больше 20"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if len(number_account) < 20:
-        raise ValueError("Количество цифр меньше 20")
+        error_message = "Количество цифр меньше 20"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if not number_account.isdigit():
-        raise ValueError("Ошибка ввода: введены не только цифры")
+        error_message = "Ошибка ввода: введены не только цифры"
+        logger.error(error_message)
+        raise ValueError(error_message)
     if len(number_account) == 20 and number_account.isdigit():
+        logger.info("Процесс маскировки номера счёта. Окончание работы программы.")
         return f"**{number_account[-4:]}"
     else:
-        return "Неверно набран номер счета. Повторите попытку."
+        error_message = "Неверно набран номер счета. Повторите попытку."
+        logger.error(error_message)
+        return error_message
 
 
 print(get_mask_card_number("3452345673454345"))
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,32 @@
 import json
+import logging
+
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("../logs/utils.log", "w", encoding="UTF-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_to_transaction_file(path_to_file: str) -> list:
     """Функция принимает на вход путь до JSON-файла
        и возвращает список словарей о транзакциях   """
+    logger.info("Начало работы программы по обработке JSON-файла.")
     try:
         with open(path_to_file, "r", encoding="Utf8") as file:
             try:
+                logger.info("Процесс обработки JSON-файла.")
                 data_transactions = json.load(file)
+                logger.info("Окончание работы программы, возврат списка словарей о транзакциях.")
                 return data_transactions
             except json.JSONDecodeError:
+                logger.error("Ошибка в преобразовании JSON-файла.Необходимо проверить формат файла.")
                 data_transactions = []
                 return data_transactions
     except FileNotFoundError:
+        logger.error("Файл не был найден, необходимо скорректировать или проверить путь.")
         data_transactions = []
         return data_transactions
 
